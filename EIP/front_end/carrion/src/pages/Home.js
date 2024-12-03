5c51469ae0e71be26ea6f80b73e62216aa365d3f
Fanks12 <99180318+Fanks12@users.noreply.github.com>
2024-12-03 14:02:39 +0100

    Add: Home page header without correct colors and main picture

---
+++ EIP/front_end/carrion/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DefaultBar from '../components/DefaultBar';
+
+function Home() {
+  return (
+    <div>
+      <DefaultBar />
+      <p>Image Bienvenue sur Carrion !</p>
+    </div>
+  );
+}
+
+export default Home;
