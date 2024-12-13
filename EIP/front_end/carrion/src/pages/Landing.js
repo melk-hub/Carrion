eb02cba8a85c61145f11bfb3d0c580471f98859c
Aurélien Lenfant <aurelien.lenfant@epitech.eu>
2024-12-12 23:56:01 -0800

    [ADD] header and first part of the landing page

--- EIP/front_end/carrion/src/pages/Landing.js
+++ EIP/front_end/carrion/src/pages/Landing.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../styles/Landing.css';
 
 function Landing() {
   const navigate = useNavigate();
@@ -13,11 +14,32 @@ function Landing() {
   };
 
   return (
-      <div>
-          <h1>Bienvenue sur Carrion</h1>
-          <button onClick={handleLoginClick}>Se connecter</button>
-          <button onClick={handleRegisterToggle}>S'enregistrer</button>
+    <div>
+      <div className="landing-background">
+        <header className="landing-header fixed-header">
+          <h1 className="landing-title">Carrion</h1>
+          <div className="landing-buttons">
+            <button onClick={handleLoginClick} className="login-button">Se connecter</button>
+            <button onClick={handleRegisterToggle} className="register-button">S'enregistrer</button>
+          </div>
+        </header>
+
+        <main className="landing-main">
+        <h2 className="landing-welcome">Bienvenue sur Carrion</h2>
+          <p className="landing-description">Explorez notre plateforme dédiée à la gestion de candidatures pour les jeunes travailleurs cherchant des stages ou des emplois.</p>
+        </main>
       </div>
+
+      <section className="services-section">
+        <h2>Nos services</h2>
+        <p>Découvrez ce que nous proposons pour simplifier votre gestion des candidatures :</p>
+        <ul>
+          <li>Suivi des candidatures</li>
+          <li>Gestion de votre profil professionnel</li>
+          <li>Rappels et notifications personnalisés</li>
+        </ul>
+      </section>
+    </div>
   );
 }
 
