438f6496670fb38acb23e5e54150ad31eaf18755
Aurélien Lenfant <aurelien.lenfant@epitech.eu>
2025-01-05 08:26:54 -0800

    [ADD] grid for applications

--- EIP/front_end/carrion/src/pages/Dashboard.js
+++ EIP/front_end/carrion/src/pages/Dashboard.js
@@ -17,33 +17,13 @@ import '../styles/Dashboard.css';
 
 //     fetchApplications();
 //   }, []);
-
-//   return (
-//     <div className="dashboard-container">
-//       {applications.map((application) => (
-//         <div key={application.id} className="dashboard-card">
-//           <img
-//             src={application.logo}
-//             alt={`${application.companyName} logo`}
-//             className="dashboard-logo"
-//           />
-//           <div className="dashboard-details">
-//             <h3 className="dashboard-company-name">{application.companyName}</h3>
-//             <p className="dashboard-job-title">{application.jobTitle}</p>
-//             <p className="dashboard-status">Statut : {application.status}</p>
-//             <p className="dashboard-date">Date de candidature : {application.applicationDate}</p>
-//           </div>
-//         </div>
-//       ))}
-//     </div>
-//   );
 // }
 
 function Dashboard() {
   const fakeDatabase = [
     {
       id: 1,
-      logo: 'https://via.placeholder.com/75',
+      logo: 'https://via.placeholder.com/100',
       companyName: 'Entreprise A',
       jobTitle: 'Développeur Frontend',
       status: 'En cours',
@@ -51,7 +31,7 @@ function Dashboard() {
     },
     {
       id: 2,
-      logo: 'https://via.placeholder.com/75',
+      logo: 'https://via.placeholder.com/100',
       companyName: 'Entreprise B',
       jobTitle: 'Ingénieur Backend',
       status: 'Acceptée',
@@ -59,7 +39,7 @@ function Dashboard() {
     },
     {
       id: 3,
-      logo: 'https://via.placeholder.com/75',
+      logo: 'https://via.placeholder.com/100',
       companyName: 'Entreprise C',
       jobTitle: 'Designer UI/UX',
       status: 'Refusée',
@@ -67,7 +47,7 @@ function Dashboard() {
     },
     {
       id: 4,
-      logo: 'https://via.placeholder.com/75',
+      logo: 'https://via.placeholder.com/100',
       companyName: 'Entreprise D',
       jobTitle: 'Développeur Backend',
       status: 'En attente de réponse',
@@ -75,7 +55,7 @@ function Dashboard() {
     },
     {
       id: 5,
-      logo: 'https://via.placeholder.com/75',
+      logo: 'https://via.placeholder.com/100',
       companyName: 'Entreprise E',
       jobTitle: 'Chef de Projet IT',
       status: 'En attente de réponse',
@@ -83,7 +63,7 @@ function Dashboard() {
     },
     {
       id: 6,
-      logo: 'https://via.placeholder.com/75',
+      logo: 'https://via.placeholder.com/100',
       companyName: 'Entreprise F',
       jobTitle: 'Consultant DevOps',
       status: 'En attente de réponse',
@@ -93,6 +73,7 @@ function Dashboard() {
 
   const [applications, setApplications] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [viewMode, setViewMode] = useState('list');
 
   useEffect(() => {
     const loadFakeData = () => {
@@ -100,7 +81,6 @@ function Dashboard() {
         setApplications(fakeDatabase);
       }, 500);
     };
-
     loadFakeData();
   }, []);
 
@@ -116,7 +96,7 @@ function Dashboard() {
 
   return (
     <div>
-      <div className="preface">
+      <div className="top-bar">
         <div className="objectives">
           <p>Objectif de la semaine: 3/10</p>
         </div>
@@ -134,52 +114,74 @@ function Dashboard() {
           />
         </div>
       </div>
-      <div className="dashboard-container">
-      {filteredApplications.length > 0 ? (
-          filteredApplications.map((application) => (
-            <div key={application.id} className="dashboard-card">
-              <img
-                src={application.logo}
-                alt={`${application.companyName} logo`}
-                className="dashboard-logo"
-              />
-              <div className="dashboard-details">
-                <h3 className="dashboard-company-name">{application.companyName}</h3>
-                <p className="dashboard-job-title">{application.jobTitle}</p>
-                <p className="dashboard-status">Statut : {application.status}</p>
-                <p className="dashboard-date">Date de candidature : {application.applicationDate}</p>
+
+      <div className="dashboard-toggle-buttons">
+        <button
+          className={`toggle-button ${viewMode === 'list' ? 'active' : ''}`}
+          onClick={() => setViewMode('list')}
+        >
+          Voir liste
+        </button>
+        <button
+          className={`toggle-button ${viewMode === 'grid' ? 'active' : ''}`}
+          onClick={() => setViewMode('grid')}
+        >
+          Voir grille
+        </button>
+      </div>
+
+      {viewMode === 'list' && (
+        <div className="dashboard-list">
+          {filteredApplications.length > 0 ? (
+            filteredApplications.map((application) => (
+              <div key={application.id} className="dashboard-list-card">
+                <img
+                  src={application.logo}
+                  alt={`${application.companyName} logo`}
+                  className="dashboard-list-logo"
+                />
+                <div className="dashboard-list-details">
+                  <h3 className="dashboard-list-company-name">{application.companyName}</h3>
+                  <p className="dashboard-list-job-title">{application.jobTitle}</p>
+                  <p className="dashboard-list-status">Statut : {application.status}</p>
+                  <p className="dashboard-list-date">Date de candidature : {application.applicationDate}</p>
+                  <button className="dashboard-list-button">Voir les détails</button>
+                </div>
               </div>
+            ))
+          ) : (
+            <p>Aucune candidature trouvée</p>
+          )}
+        </div>
+      )}
+
+      {viewMode === 'grid' && (
+        <div className="dashboard-grid">
+          {filteredApplications.length > 0 ? (
+            filteredApplications.map((application) => (
+            <div key={application.id} className="dashboard-grid-card">
+              <div className="dashboard-grid-header">
+                <img
+                  src={application.logo}
+                  alt={`${application.companyName} logo`}
+                  className="dashboard-grid-logo"
+                />
+                <div className="dashboard-grid-company-name">{application.companyName}</div>
+              </div>
+              <div className="dashboard-grid-content">
+                <h4>{application.jobTitle}</h4>
+                <hr />
+                <p>{application.status}</p>
+                <small>{application.applicationDate}</small>
+              </div>
+              <button className="dashboard-grid-button">Voir les détails</button>
             </div>
           ))
-        ) : (
-          <p>Aucune candidature trouvée</p>
-        )}
-      </div>
-      <div className="dashboard-row">
-      {filteredApplications.length > 0 ? (
-          filteredApplications.map((application) => (
-          <div key={application.id} className="dashboard-new-card">
-            <div className="card-header">
-              <img
-                src={application.logo}
-                alt={`${application.companyName} logo`}
-                className="new-card-logo"
-              />
-              <div className="new-card-company">{application.companyName}</div>
-            </div>
-            <div className="card-content">
-              <h4>{application.jobTitle}</h4>
-              <hr />
-              <p>{application.status}</p>
-              <small>{application.applicationDate}</small>
-            </div>
-            <button className="new-card-button">Voir les détails</button>
-          </div>
-        ))
-        ) : (
-          <p>Aucune candidature trouvée</p>
-        )}
-      </div>
+          ) : (
+            <p>Aucune candidature trouvée</p>
+          )}
+        </div>
+      )}
     </div>
   );
 }
