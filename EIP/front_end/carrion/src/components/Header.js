038961492271362ca4a20a69e8ad64f141544d63
Aurélien Lenfant <aurelien.lenfant@epitech.eu>
2025-02-02 18:39:50 -0800

    [ADD] new logo and modif colors

--- EIP/front_end/carrion/src/components/Header.js
+++ EIP/front_end/carrion/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/Header.css';
-import logo from '../assets/logo-carrion.png';
+import logo from '../assets/carrion_logo.png';
 
 function Header() {
   const navigate = useNavigate();
