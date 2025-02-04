69a7ba405fdeb6bf9247c3a34d394d3f49536a24
melka@hub <pascal.lin@epitech.eu>
2025-02-04 17:49:58 +0800

    [FEAT]get jobApply information

--- EIP/back_end/carrion/src/jobApply/jobApply.controller.ts
+++ EIP/back_end/carrion/src/jobApply/jobApply.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiBearerAuth,
+} from '@nestjs/swagger';
 import { JobApplyService } from './jobApply.service';
 import { JobApplyDto } from './dto/jobApply.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt/jwt-auth.guard';
 
 @ApiTags('jobApply')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
 @Controller('job-applies')
 export class JobApplyController {
   constructor(private readonly jobApplyService: JobApplyService) {}
@@ -12,7 +20,8 @@ export class JobApplyController {
   @ApiOperation({ summary: 'Get jobApply information' })
   @ApiResponse({ status: 201, description: 'Get successfully jobApply' })
   @ApiResponse({ status: 400, description: "Can't get jobApply error" })
-  async getAllJobApplies(): Promise<JobApplyDto[]> {
-    return this.jobApplyService.getAllJobApplies();
+  async getAllJobApplies(@Request() req): Promise<JobApplyDto[]> {
+    const userId = req.user.id;
+    return this.jobApplyService.getAllJobApplies(userId);
   }
 }
