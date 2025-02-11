d31fed05b3945dc58e18e278465d9d810bbfc289
Fanks12 <99180318+Fanks12@users.noreply.github.com>
2025-02-11 12:20:32 +0100

    [ADD] Login and register update without logo

--- EIP/front_end/carrion/src/pages/Login.js
+++ EIP/front_end/carrion/src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 // import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 // import MicrosoftLogin from 'react-microsoft-login';
