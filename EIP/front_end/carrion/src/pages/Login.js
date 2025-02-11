6c05efe3896e530cff0d00d98ab0e555621adb21
Aurélien Lenfant <aurelien.lenfant@epitech.eu>
2025-02-10 21:55:58 -0800

    [MOD] working api for getting applications

--- EIP/front_end/carrion/src/pages/Login.js
+++ EIP/front_end/carrion/src/pages/Login.js
@@ -32,9 +32,16 @@ function Login({ setIsAuthenticated }) {
         credentials: 'include',
       });
 
-      const data = await response;
+      const textData = await response.text();
+      let data;
+        try {
+            data = JSON.parse(textData);
+        } catch (error) {
+            data = { accessToken: textData };
+        }
 
       if (response.ok) {
+          localStorage.setItem("token", data.accessToken);
           setIsAuthenticated(true);
           navigate('/dashboard');
       } else {
