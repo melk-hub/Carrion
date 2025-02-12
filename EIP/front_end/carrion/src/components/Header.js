c848b1b5ac33fe021977ec2023574595b779482d
Aurélien Lenfant <aurelien.lenfant@epitech.eu>
2025-02-12 00:44:45 -0800

    [ADD] new logo and update the header

--- EIP/front_end/carrion/src/components/Header.js
+++ EIP/front_end/carrion/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/Header.css';
-import logo from '../assets/carrion_logo.png';
+import logo from '../assets/carrion_logo2.png';
 
 function Header() {
   const navigate = useNavigate();
@@ -9,10 +9,10 @@ function Header() {
     <header className="header">
       <h1 onClick={() => navigate('/dashboard')} className="logo-button"><img src={logo} alt="Carrion"/></h1>
       <div className="navigation-buttons">
-        <button onClick={() => navigate('/dashboard')} className='applications-button'>Mes Candidatures</button>
-        <button onClick={() => navigate('/archives')} className='archives-button'>Mes Archives</button>
-        <button onClick={() => navigate('/objectives')} className='objectives-button'>Mes Objectifs</button>
-        <button onClick={() => navigate('/parameters')} className='parameters-button'>Mes Paramètres</button>
+        <button onClick={() => navigate('/dashboard')} className='applications-button'>Candidatures</button>
+        <button onClick={() => navigate('/archives')} className='archives-button'>Archives</button>
+        <button onClick={() => navigate('/objectives')} className='objectives-button'>Objectifs</button>
+        <button onClick={() => navigate('/parameters')} className='parameters-button'>Profil</button>
       </div>
       <button onClick={() => navigate('/')} className="logout-button">Déconnexion</button>
     </header>
