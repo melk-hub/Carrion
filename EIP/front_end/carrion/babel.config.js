ad78f3a8be66e58e1cf8a16b44a3a943c1fda35a
melka@hub <pascal.lin@epitech.eu>
2025-02-13 04:01:00 +0800

    [CI]railway abort eslint warning

--- EIP/front_end/carrion/babel.config.js
+++ EIP/front_end/carrion/babel.config.js
@@ -1,6 +1,6 @@
-export default {
-    presets: [
-      '@babel/preset-env',
-      '@babel/preset-react',
-    ]
-};  
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+  ]
+};
