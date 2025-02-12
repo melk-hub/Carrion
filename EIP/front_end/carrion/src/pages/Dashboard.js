41ac61469aa0864e6c82e835f55f46ea2878f086
Aurélien Lenfant <aurelien.lenfant@epitech.eu>
2025-02-12 15:28:14 -0800

    [MOD] merge

--- EIP/front_end/carrion/src/pages/Dashboard.js
+++ EIP/front_end/carrion/src/pages/Dashboard.js
@@ -37,29 +37,6 @@ function Dashboard() {
     };
     fetchApplications();
   }, [API_URL]);
-  
-
-  useEffect(() => {
-    const fetchApplications = async () => {
-      try {
-        const response = await fetch(`${API_URL}/job-applies/jobApply`, {
-          headers: {
-            "Authorization": `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-        });
-        if (!response.ok) {
-          throw new Error(`Erreur HTTP: ${response.status}`);
-        }
-        const data = await response.json();
-        setApplications(Array.isArray(data) ? data : []);
-      } catch (error) {
-        console.error('Erreur lors de la récupération des données:', error);
-      }
-    };
-    fetchApplications();
-  }, [API_URL]);
-  
 
   const handleStatusChange = (status) => {
     setSelectedStatuses((prev) => {
