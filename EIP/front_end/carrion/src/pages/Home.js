38fa274c08a485a92ee92d110919a0131b50d3e3
Fanks12 <99180318+Fanks12@users.noreply.github.com>
2025-05-02 12:36:16 +0200

    Home Adaptation

--- EIP/front_end/carrion/src/pages/Home.js
+++ EIP/front_end/carrion/src/pages/Home.js
@@ -4,14 +4,6 @@ import '../styles/Home.css';
 function Home() {
 return (
     <div className="container">
-      <header className="header">
-        <h1>Suivi de Candidatures</h1>
-        <p>Gérez et suivez vos candidatures d'emploi en un seul endroit</p>
-        <div className="header-buttons">
-          <button>Voir le tableau de bord</button>
-          <input type="text" placeholder="Rechercher..." />
-        </div>
-      </header>
         <main className="dashboard">
         <section className="overview">
           <h2>Aperçu de vos candidatures</h2>
