534bc8bc78dbe185b05482e8d32f7fef08b7c39a
Fanks12 <99180318+Fanks12@users.noreply.github.com>
2025-05-03 12:20:51 +0200

    Navbar integrated

--- EIP/front_end/carrion/src/components/Header.js
+++ EIP/front_end/carrion/src/components/Header.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../styles/Header.css';
+import "../styles/Navbar.css";
+// import '../styles/Header.css';
 import logo from '../assets/carrion_logo.png';
 import axios from 'axios';
+import Home from '../pages/Home';
+import Dashboard from '../pages/Dashboard';
+import Archives from '../pages/Archives';
 
 function Header({ setIsAuthenticated }) {
   const navigate = useNavigate();
   const API_URL = process.env.REACT_APP_API_URL;
+  const [activeSection, setActiveSection] = useState("Accueil");
 
   const handleLogout = async () => {
     try {
@@ -19,12 +24,55 @@ function Header({ setIsAuthenticated }) {
   };
 
   return (
-    <header className="header">
+    <div className="layout">
+    <header className="topbar">
+      <div className="logo right">
+        <span className="icon" onClick={() => navigate('/home')} ><img src={logo} alt="Carrion" /></span>
+
+      </div>
+      <div className="notifications">
+        <span className="bell" onClick={handleLogout}>🔔</span>
+      </div>
+      <div className="user-profile" onClick={() => navigate('/parameters')}>
+        <img alt="User" className="avatar" />
+        <span className="username">Jeremy</span>
+      </div>
+    </header>
+
+    <div className="layout-body">
+      <nav className={`sidebar`}>
+        <ul className="menu">
+          <li onClick={() => setActiveSection("Accueil")}>
+            <span>🏠</span> {"Accueil"}
+          </li>
+          <li onClick={() => setActiveSection("Candidature")}>
+            <span>📄</span> {"Candidatures"}
+          </li>
+          <li onClick={() => setActiveSection("Archives")}>
+            <span>📊</span> {"Archives"}
+          </li>
+        </ul>
+      </nav>
+      <main className="main-content">
+      {activeSection === "Accueil" && <Home/>}
+      {activeSection === "Candidature" && <Dashboard/>}
+      {activeSection === "Archives" && <Archives/>}
+      </main>
+    </div>
+
+  </div>
+  
+  );
+}
+
+/*
+<header className="header">
   <h1 onClick={() => navigate('/dashboard')} className="logo-button">
     <img src={logo} alt="Carrion" />
   </h1>
-
+*/
   {/* Conteneur pour centrer la navigation */}
+/*
   <div className="navigation-container">
     <div className="navigation-buttons">
       <button onClick={() => navigate('/dashboard')} className='applications-button'>Candidatures</button>
@@ -36,8 +84,5 @@ function Header({ setIsAuthenticated }) {
 
   <button onClick={handleLogout} className="logout-button">Déconnexion</button>
 </header>
-
-  );
-}
-
+*/
 export default Header;
