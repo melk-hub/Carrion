79b5de0decaade12a9a402a2055a8e03ee0a9189
Fanks12 <99180318+Fanks12@users.noreply.github.com>
2025-05-09 17:02:15 +0200

    [ADD] navbar in archives

--- EIP/front_end/carrion/src/pages/Archives.js
+++ EIP/front_end/carrion/src/pages/Archives.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import '../styles/Archives.css';
+import { useNavigate } from 'react-router-dom';
+import logo from '../assets/carrion_logo_crop.png';
+import "../styles/Navbar.css";
 
 // function Archives() {
 //   const fakeDatabase = [
@@ -166,6 +169,7 @@ import '../styles/Archives.css';
 // }
 
 function Archives() {
+  const navigate = useNavigate();
   const fakeDatabase = useMemo(() => [
     {
       id: 1,
@@ -241,6 +245,39 @@ function Archives() {
   });
 
   return (
+    <div className="layout">
+          <header className="topbar">
+            <div className="notifications">
+              <span className="bell">🔔</span>
+            </div>
+            <div className="user-profile" onClick={() => navigate('/parameters')}>
+              <img alt="User" className="avatar" />
+              <span className="username">Jeremy</span>
+            </div>
+          </header>
+      
+          <div className="layout-body">
+            <nav className={`sidebar`}>
+            <div className="logo">
+              <span className="icon" onClick={() => navigate('/home')}>
+                <img src={logo} alt="Carrion" className="logo-img"/>
+                <span className="logo-text">CARRION</span>
+              </span>
+            </div>
+              <ul className="menu">
+                <li onClick={() => navigate('/home')}>
+                  <span>🏠</span> {"Accueil"}
+                </li>
+                <li onClick={() => navigate('/dashboard')}>
+                  <span>📄</span> {"Candidatures"}
+                </li>
+                <li onClick={() => navigate('/archives')}>
+                  <span>📊</span> {"Archives"}
+                </li>
+              </ul>
+            </nav>
+          </div>
+            <main className="main-content">
     <div>
       <div className="top-bar">
         <div className='archives-title'>
@@ -326,6 +363,8 @@ function Archives() {
         </div>
       )}
     </div>
+            </main>
+            </div>
   );
 }
 
