ac38e4709bf342030fe18af93cfb66ffc1ae8fe4
Aurélien Lenfant <aurelien.lenfant@epitech.eu>
2025-05-22 06:16:16 -0700

    [ADD] profile page v1

--- EIP/front_end/carrion/src/App.js
+++ EIP/front_end/carrion/src/App.js
@@ -118,6 +118,16 @@ function AppLayout() {
             )
           }
         />
+        <Route
+          path="/profile"
+          element={
+            isAuthenticated ? (
+              <Profile />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
       </Routes>
     </div>
   );
