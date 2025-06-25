8465246c3880063c437bc988af7350a261723986
Sean JABOULET <sean.jaboulet@epitech.eu>
2025-06-25 14:58:52 +0200

    [FIX] notification api routes

--- EIP/back_end/carrion/src/notification/notification.controller.ts
+++ EIP/back_end/carrion/src/notification/notification.controller.ts
@@ -1,22 +1,43 @@
-import { Controller, Post, Body, Get, Param, Patch } from '@nestjs/common';
-import { NotificationsService } from './notification.service';
+import { Controller, Get, Delete, Patch, Param, Req, UseGuards } from '@nestjs/common';
+import { NotificationService } from './notification.service';
+import { JwtAuthGuard } from '../auth/guards/jwt/jwt-auth.guard';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('notifications')
-export class NotificationsController {
-  constructor(private readonly notificationsService: NotificationsService) {}
+@UseGuards(JwtAuthGuard)
+export class NotificationController {
+  constructor(private notificationService: NotificationService) {}
 
-  @Post()
-  create(@Body() body: { userId: number; type: string; title: string; message: string; company: string }) {
-    return this.notificationsService.create(body.userId, body.type, body.title, body.message, body.company);
+  @Get()
+  @ApiOperation({ summary: 'Get user notifications' })
+  @ApiResponse({ status: 200, description: 'Successfully got user notifcations' })
+  @ApiResponse({ status: 400, description: "Can't get user notifications" })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  async getAll(@Req() req) {
+    const userId = req.user.id;
+    return this.notificationService.getUserNotifications(userId);
   }
 
-  @Get(':userId')
-  findByUser(@Param('userId') userId: number) {
-    return this.notificationsService.findByUser(userId);
+  @Patch(':id/read')
+  @ApiOperation({ summary: 'Toggle notification as read' })
+  @ApiResponse({ status: 200, description: 'Succesfully toggled notifications as read' })
+  @ApiResponse({ status: 400, description: "Can't toggle notification as read" })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  async markAsRead(@Param('id') id: string, @Req() req) {
+    const userId = req.user.id;
+    return this.notificationService.toggleReadStatus(id, userId);
   }
 
-  @Patch('read/:id')
-  markAsRead(@Param('id') id: number) {
-    return this.notificationsService.markAsRead(id);
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete a notification' })
+  @ApiResponse({ status: 200, description: 'Successfully deleted notification' })
+  @ApiResponse({ status: 400, description: "Can't delete notification" })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  async delete(@Param('id') id: string, @Req() req) {
+    const userId = req.user.id;
+    return this.notificationService.deleteNotification(id, userId);
   }
 }
