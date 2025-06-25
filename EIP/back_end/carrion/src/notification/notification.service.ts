d6d7f892ad8ae896d2dd312334406f4dd13dca0e
Fanks12 <99180318+Fanks12@users.noreply.github.com>
2025-06-25 11:07:24 +0200

    Add back mixed

--- EIP/back_end/carrion/src/notification/notification.service.ts
+++ EIP/back_end/carrion/src/notification/notification.service.ts
@@ -1,17 +1,79 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Notification } from './notification.entity';
+import {
+  IsUUID,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  IsOptional,
+  IsEnum,
+  IsDate,
+} from 'class-validator';
+
+export enum ApplicationType {
+  SUCCESS = 'APPLIED',
+  ERROR = 'PENDING',
+  INFO = 'INTERVIEW_SCHEDULED',
+  WARNING = 'TECHNICAL_TEST',
+  DEFAULT = 'DEFAULT'
+}
+
+// DTO de création d'une notification
+export class CreateNotificationDto {
+  @IsUUID()
+  id: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(80)
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(80)
+  company?: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(255)
+  message: string;
+
+  @IsEnum(ApplicationType)
+  type: ApplicationType;
+
+  @IsOptional()
+  read?: boolean = false;
+
+  @IsNotEmpty()
+  @IsDate()
+  createdAt: Date;
+}
 
 @Injectable()
 export class NotificationsService {
   constructor(
     @InjectRepository(Notification)
-    private notificationRepo: Repository<Notification>,
+    private readonly notificationRepo: Repository<Notification>,
   ) {}
 
-  async create(userId: number, type: string, title: string, message: string, company: string) {
-    const notification = this.notificationRepo.create({ userId, type, title, message, company});
+  async create(
+    userId: number,
+    type: ApplicationType,
+    title: string,
+    message: string,
+    company?: string,
+  ) {
+    const notification = this.notificationRepo.create({
+      userId,
+      type,
+      title,
+      message,
+      company,
+      read: false,
+      createdAt: new Date(),
+    });
+
     return await this.notificationRepo.save(notification);
   }
 
@@ -20,7 +82,7 @@ export class NotificationsService {
   }
 
   async markAsRead(id: number) {
-    await this.notificationRepo.update(id, { isRead: true });
+    await this.notificationRepo.update(id, { read: true });
     return { message: 'Notification marked as read' };
   }
 }
