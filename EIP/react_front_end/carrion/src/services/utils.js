8286493466bdc5fd7e57bff641a8b9024bf02dca
zvillon <zhg.villon@gmail.com>
2025-12-17 15:04:10 +0100

    [FIX] Remove react front end

--- EIP/react_front_end/carrion/src/services/utils.js
+++
@@ -1,5 +0,0 @@
-export const passwordRegEX =
-  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-
-export const emailRegEX =
-  /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/im;
